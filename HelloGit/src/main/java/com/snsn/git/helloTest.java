55b1eef0ad2dd07eacb8bd729c2e216da2de38a8
Sh3nSh3n <83444720+Sh3nSh3n@users.noreply.github.com>
2021-07-31 15:34:04 +0800

    zaixian

--- HelloGit/src/main/java/com/snsn/git/helloTest.java
+++ HelloGit/src/main/java/com/snsn/git/helloTest.java
@@ -3,5 +3,7 @@ package com.snsn.git;
 public class helloTest {
     public static void main(String[] args) {
         System.out.println("我是");
+        System.out.println("我是");
+        
     }
 }
